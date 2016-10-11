7551bc254fd9aa7a66d1616aaaedffc774d0a8c9
c650 <charlesbailey@protonmail.com>
2016-10-11 17:40:52 -0400

    finish up partner-assignment.cpp as i understood it

--- partner-assignment.cpp
+++ partner-assignment.cpp
@@ -1,3 +1,16 @@
+/*
+	partner-assignment.cpp
+	@author Charles Bailey
+	@version 0.0.1 10/11/16
+
+	Description: this program generates all possible
+	combinations of a bunch of students into a certain
+	size group. Any left-over students will be accounted
+	for as well. Someone could run this program once and
+	check it off as they go. At least, that is what this
+	program is intended to do. Who really knows, right?
+*/
+
 #include <iostream>
 #include <fstream>
 #include <cstdio>
@@ -8,28 +21,33 @@
 
 #include <stdexcept>
 
-#define PARTNER_LEN 2
+typedef std::vector<std::string> s_vec;
+typedef std::vector<size_t> size_t_vec;
+typedef const size_t c_size_t;
 
 void
-generate_partners(const std::vector<std::string>& classmates,
-	std::vector<size_t>& partners, const size_t& start,
-	const size_t& idx, const size_t& len)
+generate_partners(const s_vec& classmates, size_t_vec& partners,
+	c_size_t& start, c_size_t& idx, c_size_t& len)
 {
-	//printf("generate_partners(classmates, partners, start = %i, idx = %i, len = %i)\n", start,idx,len);
+	if (idx >= len) {
+		for(auto& p : partners)
+			std::cout << classmates[p] << " ";
+		std::cout << std::endl;
 
-	if (len <= 0) return; /* return if we don't need any more people */
+		return; /* return bcs we don't need any more people */
+	}
 
 	for (size_t i = start; i < classmates.size(); i++) {
 		partners[idx] = i;
- 		//printf("partners[%i] = %s\n", idx, partners[idx].c_str());
-		generate_partners(classmates, partners, i+1, idx+1, len-1);
+
+		generate_partners(classmates, partners, i+1, idx+1, len);
 	}
 }
 
 int main(int argc, char **argv) {
 
-	if (argc != 2) {
-		std::cout << "[!] Usage: ./partner-assignment <text file with partners>\n";
+	if (argc != 3) {
+		std::cout << "[!] Usage: ./partner-assignment <text file with students> <group size>\n";
 		exit(1);
 	}
 
@@ -41,23 +59,25 @@ int main(int argc, char **argv) {
 		exit(2);
 	}
 
-	std::vector<std::string> students;
+	size_t group_size = std::stoi(std::string(argv[2]));
+
+	s_vec students;
 	std::string tmp_student;
 	while(std::getline(fs, tmp_student))
 		students.push_back(tmp_student);
 
 	fs.close();
 
-	std::vector< size_t > group;
-	for (size_t i = 0; i < students.size(); i++) {
-		group = std::vector<size_t>(PARTNER_LEN);
+	size_t_vec group(group_size);
+	generate_partners(students, group, 0, 0, group_size);
 
-		generate_partners(students, group, i, 0, PARTNER_LEN);
-
-		for (auto& p : group) {
-			std::cout << students[p] << " ";
-		}
-		std::cout << std::endl;
-
+	/* since we may have extras, we need to generate padding groups: */
+	if (students.size() % group_size != 0) {
+		group = size_t_vec(students.size() % group_size);
+		generate_partners(students, group, 0, 0, students.size() % group_size);
 	}
+
+	std::cout << "A class of size " << students.size() << " split into "
+		<< students.size() / group_size << " groups of " << group_size
+		<< " with " << students.size() % group_size << " students left over.\n";
 }
