fd3a9c2fc280caca5e2e6f2b2198b951fc6fca25
Charles <c650@users.noreply.github.com>
2017-07-05 19:31:50 -0400

    build web version

--- partner-assignment-web/app.rb
+++ partner-assignment-web/app.rb
@@ -1,9 +1,28 @@
-require "sinatra"
 require "bundler"
-
 Bundler.require
 
 class Application < Sinatra::Base
 
+	get '/' do
+		erb :index
+	end
 
+	post '/do-assignments' do
+		tmp_file = ""
+		10.times do |t|
+			tmp_file += [rand(65..90),rand(97..122)].sample.chr
+		end
+
+		list = params[:list].split(/,|\n+/).join("\n")
+
+		tmp_file = "/tmp/" + tmp_file
+		File.open(tmp_file, "w") do |f|
+			f.write(list)
+		end
+
+		command = "../partner-assignment/make-partner-sets.out #{tmp_file} #{params[:group_size]} --as-json"
+		@results = JSON.parse(`#{command}`)
+
+		erb :index
+	end
 end
